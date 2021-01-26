d59c74e5f88c9710cc8c5c2ff1f53fa96dd8b315
Azhar <azhar.kori@gmail.com>
2021-01-26 16:26:46 +0500

    3rd

--- src/main/java/net/codejava/AppMain.java
+++ src/main/java/net/codejava/AppMain.java
@@ -1,15 +1,7 @@
 package net.codejava;
 
-import java.time.Duration;
-
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class AppMain {
